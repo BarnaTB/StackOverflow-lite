ec80dd6b72597af662339ed05b4e5b95b382901d
BarnaTB <barnabastb2@gmail.com>
2018-08-14 22:38:22 +0300

    ft(User should be  able to add an answer): Commit delivers a feature for a user to add an answer to a question on the platform. [Delivers #159763625]

--- api/routes.py
+++ api/routes.py
@@ -5,7 +5,7 @@ from api import app
 from api.models import *
 
 
-@app.route('/questions', method=['POST'])
+@app.route('/questions', methods=['POST'])
 def add_question():
     data = request.get_json()
 
@@ -16,7 +16,7 @@ def add_question():
     details = data.get('details')
 
     if not details and len(details.strip(" ")) != 0:
-        return jsonify({"message": "Please enter question details"}), 400
+        return jsonify({"message": "Missing question!"}), 400
     question = Question(question_id, details)
     questions.append(question)
 
@@ -26,20 +26,26 @@ def add_question():
     }), 201
 
 
-@app.route('/questions/<questionId>/answers', method=['POST'])
+@app.route('/questions/<questionId>/answers', methods=['POST'])
 def add_answer(questionId: int):
     data = request.get_json()
 
     questionId = data.get('questionId')
     details = data.get('details')
 
-    if not details and len(details.strip(" ")) != 0:
-        return jsonify({'message': 'Please enter an answer.'}), 400
-    if questionId > len(questions) or questionId <= len(questions):
-        return jsonify({'message': 'Question does not exist!'}), 400
-    answer = Answer(questionId, details)
-    answers.append(answer)
+    try:
+        if not details and len(details.strip(" ")) != 0:
+            return jsonify({'message': 'Please enter an answer.'}), 400
+        if questionId > len(questions) or questionId <= 0:
+            return jsonify({'message': 'Question does not exist!'}), 400
+        answer = Answer(questionId, details)
+        answers.append(answer)
 
-    return jsonify({
-        'Answer': 
-    })
+        return jsonify({
+            'Answer': answer.__dict__,
+            'Message': 'Answer added succesfully!'
+        }), 201
+    except TypeError:
+        return jsonify({
+            'message': 'question id must be a number!'
+        }), 400
