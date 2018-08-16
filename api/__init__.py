cbd3d1edd3516ac7d9b33277bd03e1355a0ee0ac
BarnaTB <barnabastb2@gmail.com>
2018-08-16 17:21:04 +0300

    ft(User should be able to fetch a single question): Commit fixes bug where routes can't be registered to the app instance and adds blueprints. [Fixes #159763508]

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
+from api.routes import mod
 
 app = Flask(__name__)
+
+app.register_blueprint(routes.mod, url_prefix=('/api/v1'))
--- api/routes.py
+++ api/routes.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 import json
 import uuid
-from api import app
+# from api import app
 from api.models import *
+from flask import Blueprint
 
 
-@app.route('/questions', methods=['POST'])
+mod = Blueprint('questions', __name__)
+
+
+@mod.route('/questions', methods=['POST'])
 def add_question():
     """
-    Method enables user to create a question by first checking if they have
+    Function enables user to create a question by first checking if they have
     entered an empty string and returns an error message in that case. If not,
     it creates a question with the information from the json object and adds
     the question to a list of qeustions called 'questions' and returns a
@@ -19,12 +23,11 @@ def add_question():
     questionId = len(questions)
     questionId += 1
 
-    question_id = data.get('questionId')
     details = data.get('details')
 
-    if not details and len(details.strip(" ")) != 0:
+    if not details or details.isspace():
         return jsonify({"message": "Missing question!"}), 400
-    question = Question(question_id, details)
+    question = Question(questionId, details)
     questions.append(question)
 
     return jsonify({
@@ -33,10 +36,10 @@ def add_question():
     }), 201
 
 
-@app.route('/questions/<questionId>/answers', methods=['POST'])
-def add_answer(questionId: int):
+@mod.route('/questions/<int:questionId>/answers', methods=['POST'])
+def add_answer(questionId):
     """
-    Method enables user to add an answer to a question on the platform by
+    Function enables user to add an answer to a question on the platform by
     first checking for an empty string in which case it returns an error
     message and then checks if the questionId corresponds to any
     entry in the list of questions, enables the user to enter an
@@ -71,28 +74,26 @@ def add_answer(questionId: int):
         }), 400
 
 
-@app.route('/questions/<questionId>', methods=['GET'])
-def get_one_question(questionId: int):
+@mod.route('/questions/<int:questionId>', methods=['GET'])
+def get_one_question(questionId):
     """
-    Method enables a user to fetch a single question from the platform
+    Function enables a user to fetch a single question from the platform
     using the questionId by checking if that id corresponds to any
     question in the list in which case it returns a success message
     with the question that has been fetched. In a case where the question
     id does not match, an error message is returned stating that the
-    question does not exist. A type error is raised in case the question
-    id passed is not an integer.
+    question does not exist.
 
     :param questionId:
-    Parameter holds an integer value of the question id. If the value is
-    not an integer value, a TypeError is raised by the method which asks
-    the user to enter a number.
+    Parameter holds an integer value of the question id which is the id
+    of the question that the user user to fetch.
     """
     try:
         if questionId > len(questions) or questionId <= 0:
             return jsonify({
                 'message': 'Question does not exist.'
             }), 400
-        question = questions[questionId]
+        question = questions[questionId - 1]
         return jsonify({
             'Question': question.__dict__,
             'message': 'Question fetched successfully'
