493c161f3506ab45aed2b9bb3ab300daab27d945
BarnaTB <barnabastb2@gmail.com>
2018-08-16 21:00:38 +0300

    Add docstring to question endpoint

--- api/routes.py
+++ api/routes.py
@@ -1,18 +1,23 @@
 from flask import Flask, request, jsonify
 import json
 import uuid
-# import app
 from api.models import *
 from flask import Blueprint
 
 
 mod = Blueprint('questions', __name__)
 
-# app = Flask(__name__)
 
 
 @mod.route('/questions', methods=['POST'])
 def add_question():
+    """
+    Function enables user to create a question by first checking if they have
+    entered an empty string and returns an error message in that case. If not,
+    it creates a question with the information from the json object and adds
+    the question to a list of qeustions called 'questions' and returns a
+    success message wuth the question that has been created.
+    """
     data = request.get_json()
 
     questionId = len(questions)
