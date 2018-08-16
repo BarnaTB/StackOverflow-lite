28022bd651ffc5ed3e10717bce29bac33190247d
BarnaTB <barnabastb2@gmail.com>
2018-08-16 14:38:56 +0300

    bg(Fix API app instance): App could not run because routes were not registered to the app instance. Restructured the app instance to routes file to fix the bug. [Fixes #159822633]

--- api/routes.py
+++ api/routes.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 import json
 import uuid
-from api import app
+# from api import app
 from api.models import *
 
+app = Flask(__name__)
 
-@app.route('/questions', methods=['POST'])
+
+@app.route('/api/v1/questions', methods=['POST'])
 def add_question():
     """
-    Method enables user to create a question by first checking if they have
+    Function enables user to create a question by first checking if they have
     entered an empty string and returns an error message in that case. If not,
     it creates a question with the information from the json object and adds
     the question to a list of qeustions called 'questions' and returns a
@@ -19,24 +21,24 @@ def add_question():
     questionId = len(questions)
     questionId += 1
 
-    question_id = data.get('questionId')
     details = data.get('details')
 
-    if not details and len(details.strip(" ")) != 0:
+    if not details or len(details.strip(" ")) != 0:
         return jsonify({"message": "Missing question!"}), 400
-    question = Question(question_id, details)
+    question = Question(questionId, details)
     questions.append(question)
 
     return jsonify({
+        "id": questionId,
         "question": question.__dict__,
         "message": "Question added successfully!"
     }), 201
 
 
-@app.route('/questions/<questionId>/answers', methods=['POST'])
-def add_answer(questionId: int):
+@app.route('/api/v1/questions/<questionId>/answers', methods=['POST'])
+def add_answer(questionId):
     """
-    Method enables user to add an answer to a question on the platform by
+    Function enables user to add an answer to a question on the platform by
     first checking for an empty string in which case it returns an error
     message and then checks if the questionId corresponds to any
     entry in the list of questions, enables the user to enter an
@@ -50,31 +52,25 @@ def add_answer(questionId: int):
     """
     data = request.get_json()
 
-    questionId = data.get('questionId')
     details = data.get('details')
 
-    try:
-        if not details and len(details.strip(" ")) != 0:
-            return jsonify({'message': 'Please enter an answer.'}), 400
-        if questionId > len(questions) or questionId <= 0:
-            return jsonify({'message': 'Question does not exist!'}), 400
-        answer = Answer(questionId, details)
-        answers.append(answer)
+    if not details and len(details.strip(" ")) != 0:
+        return jsonify({'message': 'Please enter an answer.'}), 400
+    if questionId > len(questions) or questionId <= 0:
+        return jsonify({'message': 'Question does not exist!'}), 400
+    answer = Answer(questionId, details)
+    answers.append(answer)
 
-        return jsonify({
-            'Answer': answer.__dict__,
-            'Message': 'Answer added succesfully!'
-        }), 201
-    except TypeError:
-        return jsonify({
-            'message': 'Question id must be a number!'
-        }), 400
+    return jsonify({
+        'Answer': answer.__dict__,
+        'Message': 'Answer added succesfully!'
+    }), 201
 
 
-@app.route('/questions/<questionId>', methods=['GET'])
-def get_one_question(questionId: int):
+@app.route('/api/v1/questions/<int:questionId>', methods=['GET'])
+def get_one_question(questionId):
     """
-    Method enables a user to fetch a single question from the platform
+    Function enables a user to fetch a single question from the platform
     using the questionId by checking if that id corresponds to any
     question in the list in which case it returns a success message
     with the question that has been fetched. In a case where the question
@@ -87,26 +83,27 @@ def get_one_question(questionId: int):
     not an integer value, a TypeError is raised by the method which asks
     the user to enter a number.
     """
+    questionId = int(questionId)
     try:
-        if questionId > len(questions) or questionId <= 0:
+        if len(questions) < 0:
             return jsonify({
-                'message': 'Question does not exist.'
+                'message': 'You have no questions yet.'
             }), 400
-        question = questions[questionId]
+        question = questions[questionId - 1]
         return jsonify({
             'Question': question.__dict__,
             'message': 'Question fetched successfully'
         }), 200
-    except TypeError:
+    except IndexError:
         return jsonify({
-            'message': 'Question id must be a number!'
+            'message': 'Question does not exist.'
         }), 400
 
 
-@app.route('/questions', methods=['GET'])
+@app.route('/api/v1/questions', methods=['GET'])
 def get_all_questions():
     """
-    Method enables a user to fetch all questions on the platform by checking
+    Function enables a user to fetch all questions on the platform by checking
     if the length of the questions list is not zero, in which case it returns
     an error message telling the user there are no questions in the list yet
     else, it returns all the questions in the list of questions on the
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-from api import app
+from api.routes import app
 
 app.run(debug=True)
