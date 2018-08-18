a6211c191543f71806eef78fc6cf96f21162e769
BarnaTB <barnabastb2@gmail.com>
2018-08-18 17:32:31 +0300

    Add routes to enable user login and register

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,5 @@ from api import routes
 app = Flask(__name__)
 
 app.register_blueprint(routes.mod, url_prefix='/api/v1')
-
 app.register_blueprint(routes.mod, url_prefix='/api/v1/questions')
-app.register_blueprint(routes.mod, url_prefix='/api/v1')
+app.register_blueprint(routes.mod, url_prefix='/api/v1/auth')
